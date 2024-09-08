50f64575aaa425b60e77ee63ee127bdabc3ee799
Yonas Debru Ameha <yonasdeberu12@gmail.com>
2024-09-08 23:49:42 +0300

    hey

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //when there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
+    //when player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉 Correct Number !';
+
+    displayMessage('🎉 Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //implementing the high score.
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    /// condensing the code in to dry form : the too high and the too low.
+  } 
